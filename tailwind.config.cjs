fb984196f9ef79d83b7b85f8c92b0fe273cd5737
Odyoz14 <alexcrypto14@gmail.com>
2025-04-12 14:50:59 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,22 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
+      colors: {
+        'capsy-blue': '#A3D5FF',
+        'capsy-pink': '#FDE2E4',
+        'capsy-lav': '#DCCFFB',
+        'capsy-gray': '#DDE6ED',
+      },
       fontFamily: {
         poetic: ['"Pacifico"', 'cursive'],
+        body: ['"Lora"', 'serif']
+      },
+      borderRadius: {
+        xl: '1rem',
+        '2xl': '1.5rem'
+      },
+      boxShadow: {
+        soft: '0 4px 12px rgba(0, 0, 0, 0.05)'
       },
     },
   },
